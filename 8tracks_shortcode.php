8bf9bff5159d1e924ecbf3ca54543dfa9b1a9c54
songsthatsaved <songsthatsaved@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-16 03:48:06 +0000

    0.8
    git-svn-id: https://plugins.svn.wordpress.org/8tracks-shortcode/trunk@438880 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 8tracks_shortcode.php
+++ 8tracks_shortcode.php
@@ -4,7 +4,7 @@
 Plugin Name: 8tracks Shortcode Plugin
 Plugin URI: http://wordpress.org/extend/plugins/8tracks-shortcode/
 Description: Allows you to embed 8tracks playlists via a shortcode.
-Version: 0.75
+Version: 0.8
 Author: Jonathan Martin
 Author URI: http://www.shh-listen.com
 License: GPL2 (http://www.gnu.org/licenses/gpl-2.0.html)
