a326fab0836dbde028853c9876dc44f8b7533087
songsthatsaved <songsthatsaved@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-21 05:01:56 +0000

    Slight adjustments to widget formatting.
    git-svn-id: https://plugins.svn.wordpress.org/8tracks-shortcode/trunk@656090 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widget.php
+++ widget.php
@@ -10,93 +10,66 @@ class eighttracks_widget extends WP_Widget {
 
   function widget($args, $instance) {
     extract($args);
-    $title    	= apply_filters('widget_title',$instance['title']);
+    	$title    	= apply_filters('widget_title',$instance['title']);
 	$url		= trim($instance['eighttracks_url']);
 	$height		= trim($instance['eighttracks_height']);
 	$width		= trim($instance['eighttracks_width']);
 	$flash		= trim($instance['eighttracks_flash']);
 	$tags		= trim($instance['eighttracks_tags']);
 	$artist		= trim($instance['eighttracks_artist']);
-	$dj			= trim($instance['eighttracks_dj']);
+	$dj		= trim($instance['eighttracks_dj']);
 	$mixset		= trim($instance['eighttracks_mixset']);
 	$collection	= trim($instance['eighttracks_collection']);
 	$perpage	= trim($instance['eighttracks_perpage']);
 	$sort		= trim($instance['eighttracks_sort']);
-	
-// Initializing the output code.
-	if ($url)  {
-	    $output = '';
-	
-	    $output .= $before_widget;
-	                      
-	    if ($title) {
-	      $output .= $before_title . $title . $after_title;
-	    }
-	  
-		$url_bits = parse_url( $url );
-		if ( '8tracks.com' != $url_bits['host'] )
-			return '';
-		
-		$the_body = wp_remote_get( esc_url($url) . '.xml' .'?api_key=d7ac7187f6230c9232f2d7d08dc12e4b50d1b8d8' );
-	
-		if (is_wp_error($the_body) || $the_body['response']['code'] != '200')
-			return '';
-	
-		if (!isset( $the_body['body']))
-			return '';
-	
-		try {	
-			$xml = new SimpleXMLElement($the_body['body']);	
-		} catch (Exception $e) {
-			return '';
-		}
-	
-		if ($eighttracks_url != '' or $eighttracks_tags != '' or $eighttracks_artist != '' or $eighttracks-dj != '' or $eighttracks_mixset != '') {
 
-		echo do_shortcode('[8tracks url="'.($eighttracks_url).'" mixset="'.($eighttracks_mixset).'" height="'.intval($eighttracks_height).'" width="'.intval($eighttracks_width).'" flash="'.($eighttracks_flash).'" tags="'.str_replace($badchars, $goodchars, $eighttracks_tags).'" artist="'.str_replace($badchars, $goodchars, $eighttracks_artist).'" dj="'.str_replace($badchars, $goodchars, $eighttracks_dj).'" collection="'. ($eighttracks_collection) .'" sort="' . ($eighttracks_sort) . '" perpage="' . intval($eighttracks_perpage) . '"]');
+// Initializing the output code.
+    echo ($args['before_widget']);
+    echo ($args['before_title']);
+    echo ($title);
+    echo ($args['after_title']);
+    echo '<div class="textwidget">';
+    if ($url != '' or $tags != '' or $artist != '' or $dj != '' or $mixset != '') {
+
+		echo do_shortcode('[8tracks url="'.($url).'" mixset="'.($mixset).'" height="'.intval($height).'" width="'.intval($width).'" flash="'.($flash).'" tags="'.str_replace($badchars, $goodchars, $tags).'" artist="'.str_replace($badchars, $goodchars, $artist).'" dj="'.str_replace($badchars, $goodchars, $dj).'" collection="'. ($collection) .'" sort="' . ($sort) . '" perpage="' . intval($perpage) . '"]');
     }
-		echo '</div>';
-	
-	    $output .= $after_widget;    
-	    
-		echo $output;
-	}
-	
+    echo '</div>';
+    echo ($args['after_widget']);
  }
 
   function update($new_instance, $old_instance) {
     $instance = $old_instance;
  
-    $instance['title']     				= strip_tags($new_instance['title']);
+    $instance['title']     			= strip_tags($new_instance['title']);
     $instance['eighttracks_url']  		= strip_tags($new_instance['eighttracks_url']);
-    $instance['eighttracks_flash']  	= strip_tags($new_instance['eighttracks_flash']);
-    $instance['eighttracks_height']     = strip_tags($new_instance['eighttracks_height']);
-    $instance['eighttracks_width']   	= strip_tags($new_instance['eighttracks_width']);
-	$instance['eighttracks_tags']		= strip_tags($new_instance['eighttracks_tags']);
-	$instance['eighttracks_artist']		= strip_tags($new_instance['eighttracks_artist']);
-	$instance['eighttracks_dj']			= strip_tags($new_instance['eighttracks_dj']);
-	$instance['eighttracks_mixset']		= strip_tags($new_instance['eighttracks_mixset']);
-	$instance['eighttracks_collection']	= strip_tags($new_instance['eighttracks_collection']);
-	$instance['eighttracks_perpage']	= strip_tags($new_instance['eighttracks_perpage']);
-	$instance['eighttracks_sort']		= strip_tags($new_instance['eighttracks_sort']);
+    $instance['eighttracks_flash']  		= strip_tags($new_instance['eighttracks_flash']);
+    $instance['eighttracks_height']     	= strip_tags($new_instance['eighttracks_height']);
+    $instance['eighttracks_width']   		= strip_tags($new_instance['eighttracks_width']);
+    $instance['eighttracks_tags']		= strip_tags($new_instance['eighttracks_tags']);
+    $instance['eighttracks_artist']		= strip_tags($new_instance['eighttracks_artist']);
+    $instance['eighttracks_dj']			= strip_tags($new_instance['eighttracks_dj']);
+    $instance['eighttracks_mixset']		= strip_tags($new_instance['eighttracks_mixset']);
+    $instance['eighttracks_collection']		= strip_tags($new_instance['eighttracks_collection']);
+    $instance['eighttracks_perpage']		= strip_tags($new_instance['eighttracks_perpage']);
+    $instance['eighttracks_sort']		= strip_tags($new_instance['eighttracks_sort']);
 	
 	return $instance;
   }
 
   function form($instance){
-    $defaults = array('title' => '', 'flash' => 'no', 'eighttracks_height' => '250', 'eighttracks_width' => '250');
+    $defaults = array('title' => '', 'flash' => 'no', 'height' => '250', 'width' => '250');
     $instance = wp_parse_args( (array) $instance, $defaults);
     
-    $title    	= strip_tags($instance['title']);
-    $width 		= strip_tags($instance['eighttracks_width']); 
-    $height 	= strip_tags($instance['eighttracks_height']); 
-    $url    	= strip_tags($instance['eighttracks_url']);
+    	$title    	= strip_tags($instance['title']);
+    	$width 		= strip_tags($instance['eighttracks_width']); 
+    	$height 	= strip_tags($instance['eighttracks_height']); 
+    	$url    	= strip_tags($instance['eighttracks_url']);
 	$flash 		= strip_tags($instance['eighttracks_flash']);
 	$tags 		= strip_tags($instance['eighttracks_tags']);
 	$artist		= strip_tags($instance['eighttracks_artist']);
 	$dj 		= strip_tags($instance['eighttracks_dj']);
 	$mixset 	= strip_tags($instance['eighttracks_mixset']);
-	$collection = strip_tags($instance['eighttracks_collection']);
+	$collection 	= strip_tags($instance['eighttracks_collection']);
 	$perpage 	= strip_tags($instance['eighttracks_perpage']);
 	$sort	 	= strip_tags($instance['eighttracks_sort']);
 	
